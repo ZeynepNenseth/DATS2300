0b30d1696c9e193576e4e9e69d836ef667332aca
ZeynepNenseth <70289448+ZeynepNenseth@users.noreply.github.com>
2021-08-27 14:08:45 +0200

    Initial commit

--- src/Uke1/oppg11111.java
+++ src/Uke1/oppg11111.java
@@ -8,10 +8,13 @@ import java.util.stream.IntStream;
 public class oppg11111 {
     public static void main(String ... args) {
         int[] a = {8, 3, 5, 7, 9, 6, 10, 2, 1, 4}; // en heltallstabell
+        int[] b = {}; // tom array
         IntStream s = Arrays.stream(a); // fra int-tabell til IntStream
         IntStream s1 = Arrays.stream(a); // fra int-tabell til IntStream
+        IntStream s2 = Arrays.stream(b); // kjøring gir
         OptionalInt resultat = s.max(); // kaller max-metoden
         OptionalInt resultat1 = s1.min();
+        OptionalInt resultat2 = s2.max();
 
         if (resultat.isPresent()) {
             System.out.println(resultat.getAsInt());
@@ -21,6 +24,12 @@ public class oppg11111 {
 
         if (resultat1.isPresent()) {
             System.out.println(resultat1.getAsInt());
+        } else {
+            System.out.println("Ingen verdi!");
+        }
+
+        if (resultat2.isPresent()) {
+            System.out.println(resultat2.getAsInt());
         } else {
             System.out.println("Ingen verdi!");
         }
--- src/Uke1/oppg1135.java
+++ src/Uke1/oppg1135.java
@@ -10,23 +10,27 @@ public class oppg1135 {
     public static int[] minmaks(int[] a) {
         int m1 = 0; //indeksen til det minste tallet i tabellen
         int m2 = 0; //indeksen til det største tallet i tabellen
-        int verdi = a[0]; //første verdi i tabellen
+        int verdimin = a[0]; //første verdi i tabellen
+        int verdimax = a[0];
+
+        int teller = 0; // teller hvor mange ganger du bruker if'en eller if else
 
         for (int i = 1; i < a.length; i++) {
-            if (a[i] < verdi) {
-                verdi = a[i];
+            teller += 2;
+            if (a[i] < verdimin) {
+                verdimin = a[i];
                 m1 = i;
-            }
-        }
-
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] > verdi) {
-                verdi = a[i];
+                teller--;
+            } else if (a[i] > verdimax) {
+                verdimax = a[i];
                 m2 = i;
             }
         }
+
         // TA sin måte å returnere
         // return new int[]{m1, m2};
+
+        System.out.println(teller);
         int[] b = {m1, m2};
         return b;
     }
