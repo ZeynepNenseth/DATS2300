982961b96c088998dfd3ca4b7687892a3378fd96
ZeynepNenseth <70289448+ZeynepNenseth@users.noreply.github.com>
2021-10-31 23:47:25 +0100

    Initial commit

---
+++ src/Uke10/oppg5244.java
@@ -0,0 +1,10 @@
+package Uke10;
+
+import Uke2.SBinTre;
+
+public class oppg5244 {
+    public static void main(String[] args) {
+        SBinTre<String> bokstavTre = SBinTre.balansert("ABCDEFGHIJKLMNO".split(""));       // et tomt tre
+
+    }
+}
--- src/Uke2/SBinTre.java
+++ src/Uke2/SBinTre.java
@@ -1,6 +1,10 @@
 package Uke2;
 
+import Uke8.Stakk;
+import Uke8.TabellStakk;
+
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -43,14 +47,71 @@ public class SBinTre<T> // implements Beholder<T>
         return tre;                                    // treet returneres
     }
 
+    public static <T> SBinTre<T> balansert(T[] a, Comparator<? super T> c)
+    {
+        SBinTre<T> tre = new SBinTre<>(c);          // oppretter et tomt tre
+        tre.rot = balansert(a, 0, a.length - 1);    // bruker den rekursive metoden
+        tre.antall = a.length;                      // setter antallet
+        return tre;                                 // returnerer treet
+    }
+
+    public static <T extends Comparable<? super T>> SBinTre<T> balansert(T[] a)
+    {
+        return balansert(a, Comparator.naturalOrder());
+    }
+
     public int antall()        // antall verdier i treet
     {
         return antall;
     }
 
+    private static int høyde(Node<?> p)  // ? betyr vilkårlig type
+    {
+        if (p == null) return -1;          // et tomt tre har høyde -1
+
+        return 1 + Math.max(høyde(p.venstre), høyde(p.høyre));
+    }
+
+    public int høyde()
+    {
+        return høyde(rot);                 // kaller hjelpemetoden
+    }
+
     public boolean tom()       // er treet tomt?
     {
         return antall == 0;
+    }
+
+    public boolean inneholder(T verdi)     // skal ligge i klassen SBinTre
+    {
+        if (verdi == null) return false;            // treet har ikke nullverdier
+
+        Node<T> p = rot;                            // starter i roten
+        while (p != null)                           // sjekker p
+        {
+            int cmp = comp.compare(verdi, p.verdi);   // sammenligner
+            if (cmp < 0) p = p.venstre;               // går til venstre
+            else if (cmp > 0) p = p.høyre;            // går til høyre
+            else return true;                         // cmp == 0, funnet
+        }
+        return false;                               // ikke funnet
+    }
+
+    public int antall(T verdi) {
+        Node<T> p = rot;
+        int teller = 0;
+         while(p != null) {
+             int cmp = comp.compare(verdi, p.verdi);
+             if (cmp < 0) {
+                 p = p.venstre;
+             } else {
+                 if (cmp == 0) {
+                     teller++;
+                 }
+                 p = p.høyre;
+             }
+         }
+         return teller;
     }
 
     public final boolean leggInn(T verdi) {    // skal ligge i class SBinTre
@@ -80,6 +141,82 @@ public class SBinTre<T> // implements Beholder<T>
 
         antall++;                                // én verdi mer i treet
         return true;                             // vellykket innlegging
+    }
+
+    public Liste<T> intervallsøk(T fraverdi, T tilverdi) {
+        Liste<T> tabell = new TabellListe<T>();
+        Stakk<Node<T>> s = new TabellStakk<>();
+
+        Node<T> p = rot;
+
+        while (p != null) {    // leter etter fraverdi
+            int cmp = comp.compare(fraverdi, p.verdi);
+
+            if (cmp < 0) {
+                s.leggInn(p); p = p.venstre;
+            } else if (cmp > 0) p = p.høyre;
+            else break;                      // vi har funnet verdien
+        }
+
+        if (p == null) p = s.taUt();  // neste i inorden = fjern verdien
+
+        while (p != null && comp.compare(p.verdi, tilverdi) < 0) {
+            tabell.leggInn(p.verdi);
+            if (p.høyre != null)
+            {
+                p = p.høyre;
+                while (p.venstre != null)
+                {
+                    s.leggInn(p); p = p.venstre;
+                }
+            }
+            else if (!s.tom()) p = s.taUt();
+            else p = null;
+        }
+        return tabell;
+    }
+
+    public T maks() {
+        if (tom()) {
+            throw new NoSuchElementException("treet er tomt");
+        }
+
+        Node<T> p = rot;
+        while (p.høyre != null) {
+            p = p.høyre;
+        }
+        return p.verdi;
+    }
+
+    public T maksFirst() {
+        if (tom()) {
+            throw new NoSuchElementException("treet er tomt");
+        }
+
+        Node<T> p = rot;
+        Node<T> q = null;
+        while (p.høyre != null) {
+            p = p.høyre;
+            if (comp.compare(p.verdi, q.verdi) > 0) {
+                q = p;
+            }
+        }
+        return q.verdi;
+    }
+
+    private static <T> Node<T> balansert(T[] a, int v, int h)  // en rekursiv metode
+    {
+        if (v > h) return null;                       // tomt intervall -> tomt tre
+
+        int m = (v + h)/2;                            // midten
+        T verdi = a[m];                               // midtverdien
+
+        while (v < m && verdi.equals(a[m-1])) m--;    // til venstre
+
+        Node<T> p = balansert(a, v, m - 1);           // venstre subtre
+        Node<T> q = balansert(a, m + 1, h);           // høyre subtre
+
+        return new Node<>(verdi, p, q);               // rotnoden
     }
 
     public void nullstill()
